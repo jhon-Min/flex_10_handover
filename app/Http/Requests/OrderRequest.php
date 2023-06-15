1a78e79f095941b6ec7a9f3e0a49913d3fa547a6
kyawzinhtet <KyawZinHtet200116@outlook.com>
2023-06-15 21:39:05 +0630

    complete form of order request

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class OrderRequest extends FormRequest
 {
@@ -21,11 +24,15 @@ class OrderRequest extends FormRequest
      */
     public function rules(): array
     {
-        $id = $this->route('order')->id;
-        dd($id);
+        // $id = $this->route('order')->id;
+        // dd($id);
         return [
-            "order" => "required|integer|exists:orders,id,$id",
+            "order" => "required|integer|exists:orders,id",
             "order_status" => "required|in:0,1,2,3,4",
         ];
+    }
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(['message' => implode('-', $validator->errors()->all())], 401));
     }
 }
