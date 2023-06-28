b9238afb7d39036818cdf890b49a4d6a19715314
jhon-Min <minnyisay@gmail.com>
2023-06-28 22:05:57 +0630

    trying to implement product api

--- app/Repositories/ProductsRepository.php
+++ app/Repositories/ProductsRepository.php
@@ -47,7 +47,12 @@ class ProductsRepository extends BaseRepository
 
     public function getProducts($filters = array(), $paginate = FALSE, $page = 1, $per_page = 50)
     {
-        $product_api = $this->importProducts();
+        $this->partsdbapirepository->login();
+        try {
+            $product_api = $this->importProducts();
+        } catch (\Throwable $th) {
+            return $th->getTraceAsString();
+        }
         $products = Product::with(['brand', 'vehicles', 'vehicles.make', 'vehicles.model', 'images', 'categories', 'criteria']);
         $products->CompanyWebStatus();
 
